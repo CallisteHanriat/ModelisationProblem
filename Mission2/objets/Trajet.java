de79c9be0617b962ccc43de0b1a262fdf014a499
Calliste Hanriat <couim38@gmail.com>
2018-02-07 12:01:19 +0100

    nouveaux fichiers

---
+++ Mission2/objets/Trajet.java
@@ -0,0 +1,20 @@
+package objets;
+
+public class Trajet {
+	int duration;
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public Trajet(int duration) {
+		super();
+		this.duration = duration;
+	}
+	
+	
+}
